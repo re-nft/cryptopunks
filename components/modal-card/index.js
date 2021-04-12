3e317b42dc9c33ba3ac9464409c7ba7026535446
nazariyv <nazariy@inbox.ru>
2021-04-12 14:18:42 +0100

    icons

--- components/modal-card/index.js
+++ components/modal-card/index.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import blockies from 'ethereum-blockies';
 
 import PunkContext from '../../contexts/punk';
 import useComponentVisible from '../../hooks/useComponentVisible';
@@ -9,7 +10,17 @@ import { getTenant } from '../../utils';
 
 const TableRow = ({ address, start, end }) => {
   const isActive = Math.round(Date.now() / 1000) < end;
-  const icon = blockies.create({ seed: address });
+  const icon = blockies.create({
+    seed: address,
+    color: '#dfe', // to manually specify the icon color, default: random
+    bgcolor: '#aaa', // choose a different background color, default: random
+    size: 15, // width/height of the icon in blocks, default: 8
+    scale: 3, // width/height of each block in pixels, default: 4
+    spotcolor: '#000'
+  }).toDataURL(); // each pixel has a 13% chance of being of a third color,
+    // default: random. Set to -1 to disable it. These "spots" create structures
+    // that look like eyes, mouths and noses.  });
+  // console.log('icon')
   return (
     <tr>
       <td className="px-6 py-4 whitespace-nowrap">
