6f73535ea7c03f661d3f9fdd6563e6e955d03abb
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-04-14 00:22:04 +0530

    bugfix: hide hamburger behind modal to acheive complete overlay

--- components/header/index.js
+++ components/header/index.js
@@ -32,7 +32,7 @@ export default function Header({ activeTab }) {
 
   return (
     <>
-      <div className="relative bg-white">
+      <div style={{ zIndex: 1 }} className="relative bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6">
           <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
             <div className="flex justify-start lg:w-0 lg:flex-1">
@@ -190,10 +190,7 @@ export default function Header({ activeTab }) {
                                   {address && (
                                     <>
                                       <div>{short(address)}</div> &nbsp;{' '}
-                                      <img
-                                        className="m-auto"
-                                        src={icon}
-                                      />
+                                      <img className="m-auto" src={icon} />
                                     </>
                                   )}
                                 </div>
